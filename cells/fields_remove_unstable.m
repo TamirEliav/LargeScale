e15cf2ab0447bdd1e6c53a67e74e3cb98b26bf81
TamirEliav <tamir.eliav@gmail.com>
2018-12-05 10:54:28 +0200

    bug fix (in remove unstable fields)

--- cells/fields_remove_unstable.m
+++ cells/fields_remove_unstable.m
@@ -1,21 +1,22 @@
 function fields = fields_remove_unstable(fields, prm)
 
-if isempty(fields)==0
+if isempty(fields)
     return
 end
 
+unstable_fields_IX = [];
 for ii_field = 1:length(fields)
     num_flights_thr = max([prm.fields.min_flights_with_spikes, ...
                            prm.fields.min_flights_with_spikes_prc * fields(ii_field).FE_field_pass_num]);
 
 
-    if [fields.num_flights_with_spikes] < num_flights_thr;
+    if fields(ii_field).num_flights_with_spikes < num_flights_thr
         unstable_fields_IX = [unstable_fields_IX ii_field];
     end
 
 end
 
-fields(unstable_fields) = [];
+fields(unstable_fields_IX) = [];
 
 end
     
