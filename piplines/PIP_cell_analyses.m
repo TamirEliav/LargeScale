274d10486694984f2fdad9d4636d8312f79ad2c3
Tamir Eliav <tamir.eliav@gmail.com>
2018-12-09 23:11:33 +0200

    fix merge

--- piplines/PIP_cell_analyses.m
+++ piplines/PIP_cell_analyses.m
@@ -18,17 +18,19 @@ for ii_cell = 1:height(cells)
         
 %     cell_create_details(cell_ID);
 %     cell_create_spikes_data(cell_ID);
-    cell_calc_time_stability(cell_ID);
+%     cell_calc_time_stability(cell_ID);
     cell_create_flight_data(cell_ID);
     cell_calc_FR_map(cell_ID);
+    cell_calc_FR_map_shuffles(cell_ID);
     cell_calc_Ipos(cell_ID);
     cell_calc_fields(cell_ID);
+    cell_calc_significant(cell_ID);
     cell_plot_map_fields(cell_ID);
     
     catch err
         disp(err)
     end
     
-    close all
+%     close all
     
 end
