d3b152a53cbc8e1c89eef50766ea0fabedc15a21
tamir_eliav <tamir.eliav@gmail.com>
2025-01-15 16:01:41 +0200

    add replay directionality variant (resmapling) to supp

--- replay/paper_replay_fig_supp_7.m
+++ replay/paper_replay_fig_supp_7.m
@@ -13,7 +13,7 @@ params_opt = 11; % decoding opt
 %% define output files
 res_dir =  'L:\paper_replay\figures';
 mkdir(res_dir)
-fig_name_str = 'Figure S9';
+fig_name_str = 'Figure_S9';
 fig_caption_str = '';
 log_name_str = [fig_name_str '_log_file' '.txt'];
 log_name_str = strrep(log_name_str , ':', '-');
@@ -52,11 +52,112 @@ set(groot,  'defaultAxesTickDirMode', 'manual');
 annotation('textbox', [0.5 1 0 0], 'String',fig_name_str, 'HorizontalAlignment','center','Interpreter','none', 'FitBoxToText','on');
 
 % create panels
-panels{1}(1) = axes('position', [3 15 3 3]);
-panels{2}(1) = axes('position', [8 15 3 3]);
+panels{1}(1) = axes('position', [3 20 4 3.5]);
+panels{2}(1) = axes('position', [3 15 3 3]);
+panels{2}(2) = axes('position', [8 15 3 3]);
+
+%% replay directionality bias - plot trend over exposure to enviroenment (resampled version)
+load('E:\Tamir\work\PROJECTS\LargeScale\paper_replay\figures\replay_directionality.mat')
+% clrs = [epoch_type_clrs,'k'];
+% surprise_clipping = 50;
+for ii_epoch_type = 3%1:length(epoch_types)
+    axes(panels{1}(1));
+    cla reset
+    hold on
+    yline(0,'-k');
+%     c = clrs{ii_epoch_type};
+    x = T.session_num_from_exposure;
+    y = directionality_binom_surprise2(ii_epoch_type,:);
+%     y(~ismember(T.batNum,novelty_exposure_bats)) = nan;
+    y(nSeqs(ii_epoch_type,:)<minSeqsThr) = nan;
+%     y(y>surprise_clipping) = surprise_clipping;
+%    y(y<-surprise_clipping) = -surprise_clipping;
+%    ylimits = [-1 1]*surprise_clipping;
+   ylimits = [-1 1]*10;
+    for ii_bat = 1:length(bats)
+        bat_num = bats(ii_bat);
+        c = bats_clr_map(bat_num);
+        IX = T.bat_num==bat_num;
+        xx = x(IX);
+        yy = y(IX);
+        invalid = isnan(yy);
+        xx(invalid) = [];
+        yy(invalid) = [];
+        plot(xx,yy,'o-','color',c,'DisplayName',"bat "+bat_num,'MarkerFaceColor',c,'MarkerSize',2);
+    end
+
+%     nPointsSmooth = 3;
+%     k = (nPointsSmooth-1)/2;
+%     xi = [-1 1].*k + [(1-k):(max(x)+k)]';
+%     xx=[];
+%     yy=[];
+%     for ii_xi = 1:size(xi,1)
+%         TF = x>=xi(ii_xi,1) & x<=xi(ii_xi,2);
+%         xx(ii_xi) = nanmedian(x(TF));
+%         yy(ii_xi) = nanmedian(y(TF));
+%     end
+%     plot(xx,yy,'-','Color',c);
+    ylim(ylimits)
+    ylabel({'Replay directionality index';'(resampled data)'})
+%     text(0.5,0.8,epoch_types{ii_epoch_type},'Units','normalized')
+end
+hax=gca;
+text(.5, 2.8, {'Replay directionality';'all sessions'},'FontSize',9,'HorizontalAlignment','center','Units','normalized')
+% legend
+% x = [0.3 2]+32;
+% y = linspace(0.35,0,3)+0.6;
+% plot(x,y(1)*[1 1],'Color',clrs{1},'LineWidth',1.5,'Clipping','off')
+% plot(x,y(2)*[1 1],'Color',clrs{2},'LineWidth',1.5,'Clipping','off')
+% plot(x,y(3)*[1 1],'Color',clrs{3},'LineWidth',1.5,'Clipping','off')
+% x = x(end)+1;
+% text(x,y(1), "Sleep", 'FontSize',7)
+% text(x,y(2), "Awake", 'FontSize',7)
+% text(x,y(3), "Combined", 'FontSize',7)
+xlabel('Session no.','Units','normalized','Position',[0.5 -0.05]);
+ylim([-1 1]*7)
+yticks([-5 0 5])
+xticks([1 40])
+hax=gca;
+hax.XRuler.TickLength(1) = 0.02;
+hax.YRuler.TickLength(1) = 0.035;
+hax.XRuler.TickLabelGapOffset = -1;
+hax.YRuler.TickLabelGapOffset = 0;
+% text(5.5,-0.75,"\leftarrow"+"session #"+example_session_num_form_exposure,'FontSize',8)
+h=annotation('textarrow');
+h.Parent=hax;
+h.X = [6 5];
+h.Y = [1 1]*3.5;
+h.String = {'  SAS=4'};
+h.FontSize = 7; h.HeadLength = 4; h.HeadWidth = 3; h.HeadStyle = 'cback2';
+h.HorizontalAlignment = 'left';
+h.VerticalAlignment = 'middle';
+h=annotation('textarrow');
+h.Parent=hax;
+h.X = 16 + [0 0.1];
+h.Y = -5.65 + [0 0];
+h.String = {'SAS=8   '};
+h.FontSize = 7; h.HeadLength = 4; h.HeadWidth = 3; h.HeadStyle = 'cback2';
+h.HorizontalAlignment = 'right';
+h.VerticalAlignment = 'middle';
+h=annotation('textarrow');
+h.Parent=hax;
+h.X = 25 + [0 1];
+h.Y = -4 + [0 0];
+h.String = {'SAS=27 '};
+h.FontSize = 7; h.HeadLength = 4; h.HeadWidth = 3; h.HeadStyle = 'cback2';
+h.HorizontalAlignment = 'right';
+h.VerticalAlignment = 'middle';
+h=annotation('textarrow');
+h.Parent=hax;
+h.X = 30 + [0 -0.1];
+h.Y = -5.8 + [0 0];
+h.String = {'   SAS=19'};
+h.FontSize = 7; h.HeadLength = 4; h.HeadWidth = 3; h.HeadStyle = 'cback2';
+h.HorizontalAlignment = 'left';
+h.VerticalAlignment = 'middle';
 
 %% Main scatter plot
-axes(panels{1}(1))
+axes(panels{2}(1))
 hold on
 % data_filename = 'F:\sequences\figures\replay_vs_crossover\replay_vs_crossover_opt_11_replay position between 25-115.mat';
 data_filename = 'F:\sequences\figures\replay_vs_crossover\replay_vs_crossover_opt_11_replay position between 25-115 & same map.mat';
@@ -91,7 +192,7 @@ hax.XRuler.TickLabelGapOffset = -.5;
 hax.YRuler.TickLabelGapOffset = 1;
 
 %% Scatter plot (control - next crossover)
-axes(panels{2}(1))
+axes(panels{2}(2))
 cla
 hold on
 X = [data.seqs_all.next_co_pos];
@@ -128,6 +229,8 @@ axes(panels{1}(1))
 text(-0.35,1.12, 'A', 'Units','normalized','FontWeight','bold','FontSize',font_size);
 axes(panels{2}(1))
 text(-0.35,1.12, 'B', 'Units','normalized','FontWeight','bold','FontSize',font_size);
+axes(panels{2}(2))
+text(-0.35,1.12, 'C', 'Units','normalized','FontWeight','bold','FontSize',font_size);
 
 %%
 fig_name = sprintf('%s',fig_name_str);
